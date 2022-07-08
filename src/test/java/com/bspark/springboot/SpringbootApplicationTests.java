e6dea1f0a4d83376820492bb87d1e83787c5bd6f
jpark6 <qkstjr09@gmail.com>
2022-07-08 16:10:33 +0900

    Spring Boot Project Init Commit

---
+++ src/test/java/com/bspark/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bspark.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
